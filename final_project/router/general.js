9db75562a71920ae0be324f22b20ccc7b49c2d5a
charles <mo.charles2019@gmail.com>
2024-02-01 11:26:31 -0500

    filter to get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  return res.status(300).json(Object.values(books).filter(
+    book => {return (book.author == author)}));
 });
 
 // Get all books based on title
