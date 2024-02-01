c6496d4e06bb11fd3570e1eccd549ca542ebcdda
charles <mo.charles2019@gmail.com>
2024-02-01 14:34:58 -0500

    add doesExist helper function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,16 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
