1e4a5258509c4d5066fee2201622de570e393bbf
charles <mo.charles2019@gmail.com>
2024-02-04 14:47:42 -0500

    add review based on username and isbn

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -53,7 +53,11 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  const review = req.query.review;
+  books[isbn].reviews[username] = {"username": username, "review": review};
+  return res.status(300).json({message: "Successfully submitted review"});
 });
 
 module.exports.authenticated = regd_users;
